aeebd799b0058698daf91994db9ac1357832fa1c
zhiyang <zhiyang3370@dingtalk.com>
2020-10-10 08:52:12 +0800

    添加冒泡排序

---
+++ index.js
@@ -0,0 +1,42 @@
+const arr = [2, 4, 6, 1, 3, 5, 8, 9, 7];
+
+// 冒泡排序经过每一轮的比较会把当前最值交换到最后面
+// 2,4,6,1,3,5,8,9,7
+
+/**
+ * 比较
+ * @param {*} a 
+ * @param {*} b 
+ */
+function compare(a, b) {
+    return a > b;
+}
+
+/**
+ * 交换
+ * @param {*} arr 
+ * @param {*} i 
+ * @param {*} j 
+ */
+function exchange(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+/**
+ * 排序
+ * @param {*} arr 
+ */
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            if (compare(arr[j], arr[j + 1])) {
+                exchange(arr, j, j + 1);
+            }
+        }
+    }
+}
+
+bubbleSort(arr);
+console.log(arr);
