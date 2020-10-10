eb1c7c92fd5c92dfeb736468c449626448abbdb4
zhiyang <zhiyang3370@dingtalk.com>
2020-10-10 09:16:36 +0800

    添加选择排序

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const arr = [2, 4, 6, 1, 3, 5, 8, 9, 7];
 
-// 冒泡排序经过每一轮的比较会把当前最值交换到最后面
+// 选择排序经过每一轮的比较会把当前最值交换到最后面
+// 冒泡排序每次比较都会交换，而选择排序是每次比较只记录最值的索引，比较完后每一轮只交换一次
 // 2,4,6,1,3,5,8,9,7
 
 /**
@@ -28,15 +29,19 @@ function exchange(arr, i, j) {
  * 排序
  * @param {*} arr 
  */
-function bubbleSort(arr) {
+function selectSort(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - i; j++) {
+        let key = -1;
+        for (let j = 0; j < arr.length - i - 1; j++) {
             if (compare(arr[j], arr[j + 1])) {
-                exchange(arr, j, j + 1);
+                key = j;
             }
+        }
+        if(key !== -1){
+            exchange(arr, key, arr.length - i);
         }
     }
 }
 
-bubbleSort(arr);
+selectSort(arr);
 console.log(arr);
