01788de6427bd32c8f65a416dad1ac79182c393b
zhiyang <zhiyang3370@dingtalk.com>
2020-10-10 09:26:25 +0800

    完善冒泡排序

--- index.js
+++ index.js
@@ -29,8 +29,11 @@ function exchange(arr, i, j) {
  * @param {*} arr 
  */
 function bubbleSort(arr) {
+    if(!arr || arr.length <= 1) {
+        return;
+    }
     for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - i; j++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
             if (compare(arr[j], arr[j + 1])) {
                 exchange(arr, j, j + 1);
             }
