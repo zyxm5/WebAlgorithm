ab4781ec07d5fdc32fef9e0a3605cb6e9d63861e
zhiyang <zhiyang3370@dingtalk.com>
2020-10-11 14:51:26 +0800

    添加二叉树的深度优先搜索

--- index.js
+++ index.js
@@ -20,93 +20,18 @@ c.left = f;
 c.right = g;
 
 /**
- * 前序遍历
+ * 深度优先搜索
  * @param {*} root 
+ * @param {*} value 
  */
-function sort1(root) {
-    if (!root) {
+function deepSearch(root, value){
+    if(!root || !value){
         return;
     }
-    console.log(root.value);
-    sort1(root.left);
-    sort1(root.right);
-}
-
-/**
- * 中序遍历
- * @param {*} root 
- */
-function sort2(root) {
-    if (!root) {
-        return;
+    if(root.value === value){
+        return root;
     }
-    sort2(root.left);
-    console.log(root.value);
-    sort2(root.right);
+    return deepSearch(root.left, value) || deepSearch(root.right, value);
 }
 
-/**
- * 后续遍历
- * @param {*} root 
- */
-function sort3(root) {
-    if (!root) {
-        return;
-    }
-    sort3(root.left);
-    sort3(root.right);
-    console.log(root.value);
-}
-
-// sort1(a); a b d e c f g
-// sort2(a); d b e a f c g
-// sort3(a); d e b f g c a
-
-// 前序遍历结果
-const arr1 = ['a', 'b', 'd', 'e', 'c', 'f', 'g'];
-// 中序遍历结果
-const arr2 = ['d', 'b', 'e', 'a', 'f', 'c', 'g'];
-// 后序遍历结果
-const arr3 = ['d', 'e', 'b', 'f', 'g', 'c', 'a'];
-
-/**
- * 根据前序遍历和后续遍历的结果还原二叉树
- * @param {*} arr1 
- * @param {*} arr2 
- */
-function test(arr1, arr2) {
-    // 严谨性判断
-    if(!arr1 || !arr2 || arr1.length === 0 || arr2.length === 0 || arr1.length !== arr2.length){
-        return null;
-    }
-    const root = new Node(arr1[0]);
-    const index = arr2.indexOf(arr1[0]);
-    const arr2Left = arr2.slice(0, index);
-    const arr2Right = arr2.slice(index + 1);
-    const arr1Left = arr1.slice(1, arr2Left.length + 1);
-    const arr1Right = arr1.slice(arr1Left.length + 1);
-    root.left = test(arr1Left, arr2Left);
-    root.right = test(arr1Right, arr2Right);
-    return root;
-}
-/**
- * 根据后序遍历和后续遍历的结果还原二叉树
- * @param {*} arr1 
- * @param {*} arr2 
- */
-function test2(arr1, arr2) {
-    // 严谨性判断
-    if(!arr1 || !arr2 || arr1.length === 0 || arr2.length === 0 || arr1.length !== arr2.length){
-        return null;
-    }
-    const root = new Node(arr1[arr1.length - 1]);
-    const index = arr2.indexOf(arr1[arr1.length - 1]);
-    const arr2Left = arr2.slice(0, index);
-    const arr2Right = arr2.slice(index + 1);
-    const arr1Left = arr1.slice(0, arr2Left.length);
-    const arr1Right = arr1.slice(arr1Left.length, -1);
-    root.left = test2(arr1Left, arr2Left);
-    root.right = test2(arr1Right, arr2Right);
-    return root;
-}
-console.log(test2(arr3,arr2));
+console.log(deepSearch(a, 'n'));
