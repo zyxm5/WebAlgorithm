844f34773ffad94b4527370c319fe29d2ebabf38
zhiyang <zhiyang3370@dingtalk.com>
2020-10-13 16:38:39 +0800

    链表逆置

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function Node(value) {
     this.value = value;
-    this.neighbors = [];
+    this.next = null;
 }
 
 const a = new Node('a');
@@ -9,77 +9,36 @@ const c = new Node('c');
 const d = new Node('d');
 const e = new Node('e');
 
-// 需要连接的节点
-const pointSet = [a, b, c, d, e];
-// 最大代价
-const max = 100000;
-// 各个节点连接的代价
-const distance = [
-    [0, 4, 7, max, max],
-    [4, 0, 8, 6, max],
-    [7, 8, 0, 5, max],
-    [max, 6, 5, 0, 7],
-    [max, max, max, 7, 0]
-]
-
-function findIndex(pointSet, nowPoint){
-    for(let i = 0; i < pointSet.length; i ++){
-        if(pointSet[i].value === nowPoint.value){
-            return i;
-        }
-    }
-}
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
 
 /**
- * 返回以当前节点集合为起点的最小代价的终点
- * @param {*} pointSet 
- * @param {*} distance 
- * @param {*} nowPointSet 
+ * 逆置链表
+ * @param {*} root 
  */
-function getMinDisPoint(pointSet, distance, nowPointSet){
-    // 当前起点
-    let fromPoint = null;
-    // 最小代价终点
-    let minDisPoint = null;
-    // 当前需要花费的最小代价
-    let minDis = max;
-    // 遍历当前节点集合
-    for(let i = 0;i < nowPointSet.length; i++){
-        fromPoint = nowPointSet[i];
-        // 找到以当前节点为起点的所有终点所需的代价
-        const index = findIndex(pointSet, nowPointSet[i]);
-        // 遍历代价，找到最小代价
-        for(let j = 0; j < distance[index].length; j ++){
-            // 该节点不在当前节点中且代价最小
-            if(nowPointSet.indexOf(pointSet[j]) < 0 && distance[index][j] < minDis){
-                minDis = distance[index][j];
-                minDisPoint = pointSet[j];
-            }
-        }
+function niZhi(root){
+    if(root == null){
+        return;
     }
-    fromPoint.neighbors.push(minDisPoint);
-    minDisPoint.neighbors.push(fromPoint);
-    return minDisPoint;
+    // 取出倒数第二项
+    if(root.next.next == null){
+        // 将最后一项的next赋值为倒数第二项
+        root.next.next = root;
+        // 返回最后一项
+        return root.next;
+        // root.next = null;
+    }
+    // 其他项
+    const result = niZhi(root.next);
+    // 下一项的next赋值为该项
+    root.next.next = root;
+    // 该项的next赋值为null
+    root.next = null;
+    return result;
 }
 
-/**
- * 返回代价最少的节点连接-普利姆算法(加点法)
- * @param {*} pointSet 
- * @param {*} distance 
- * @param {*} start 
- */
-function prim(pointSet, distance, start){
-    const nowPointSet = [];
-    nowPointSet.push(start);
-    while(true){
-        const minDisPoint = getMinDisPoint(pointSet, distance, nowPointSet);
-        nowPointSet.push(minDisPoint);
-        if(nowPointSet.length === pointSet.length){
-            break;
-        }
-    }
-    return nowPointSet;
-}
-
-console.log(prim(pointSet, distance, pointSet[0]));
+const last = niZhi(a);
+console.log(last);
 
