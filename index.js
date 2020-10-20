862284d824d5ff4cabcd543311180c9a8651587a
zhiyang <zhiyang3370@dingtalk.com>
2020-10-21 07:00:34 +0800

    使用缓存优化

--- index.js
+++ index.js
@@ -13,19 +13,29 @@
  * 普通青蛙跳
  * @param {*} n 
  */
-function jump(n){
-    // 严谨性判断
-    if(n <= 0){
-        return -1;
+function jump(n) {
+    const cache = {};
+
+    function _jump(n) {
+        if (cache[n]) {
+            return cache[n];
+        }
+        let result;
+        // 严谨性判断
+        if (n <= 0) {
+            result = -1;
+        } else if (n == 1) {
+            result = 1;
+        } else if (n == 2) {
+            // 跳两次一个台阶或者一次两个台阶
+            result = 2;
+        } else {
+            result = _jump(n - 1) + _jump(n - 2);
+        }
+        cache[n] = result;
+        return result;
     }
-    if(n == 1){
-        return 1;
-    }
-    if(n == 2){
-        // 跳两次一个台阶或者一次两个台阶
-        return 2;
-    }
-    return jump(n - 1) + jump(n - 2);
+    return _jump(n);
 }
 
 // 变态青蛙跳台阶问题
@@ -35,23 +45,34 @@ function jump(n){
 // 同上,推理得通式:f(n) = f(n - 1) + f(n - 2) + ... + f(2) + f(1) + 1;
 // 最后的1代表一次跳上n级台阶
 
-function jump2(n){
-    // 严谨性判断
-    if(n <= 0){
-        return -1;
+function jump2(n) {
+    const cache = {};
+
+    function _jump(n) {
+        if (cache[n]) {
+            return cache[n];
+        }
+        let result;
+        // 严谨性判断
+        if (n <= 0) {
+            result = -1;
+        } else
+        if (n == 1) {
+            result = 1;
+        } else
+        if (n == 2) {
+            // 跳两次一个台阶或者一次两个台阶
+            result = 2;
+        } else {
+            result = 1; // 代表一次性跳上去
+            for (let i = 1; i < n; i++) {
+                result += _jump(i);
+            }
+        }
+        cache[n] = result;
+        return result;
     }
-    if(n == 1){
-        return 1;
-    }
-    if(n == 2){
-        // 跳两次一个台阶或者一次两个台阶
-        return 2;
-    }
-    let sum = 1;// 代表一次性跳上去
-    for(let i = 1; i < n; i ++){
-        sum += jump2(i);
-    }
-    return sum;
+    return _jump(n);
 }
 
 console.log(jump2(4));
